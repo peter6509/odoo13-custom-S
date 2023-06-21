cf4ee9b5029bf3a269648d7e5d5c70bcb04c3d94
Peter Wu <peter6509@gmail.com>
2023-06-21 10:08:18 +0800

    6-21 update

--- neweb_contract_ext/models/__init__.py
+++ neweb_contract_ext/models/__init__.py
@@ -7,6 +7,7 @@ from . import neweb_contract_inherit
 from . import neweb_contract_inherit2
 from . import neweb_contract_inherit3
 from . import neweb_contract_inherit4
+from . import neweb_contract_selline1
 from . import neweb_contract_trigger
 
 
---
+++ neweb_contract_ext/models/neweb_contract_selline1.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Author : Peter Wu
+
+from odoo import models,fields,api
+from odoo.exceptions import UserError
+
+class NewConSelcontract(models.Model):
+    _name = "neweb_contract.contract_sel"
+    _description = "舊約勾選項目HEAD"
+
+    sel_line = fields.One2many('neweb_contract.contract_selline', 'sel_id', string="勾選項目LINE")
+    contract_id = fields.Integer(string="合約ID")
+
+    def allsel_button(self):
+        myrec = self.env['neweb_contract.contract_selline'].search([])
+        myrec.selitem=True
+
+    def allnosel_button(self):
+        myrec = self.env['neweb_contract.contract_selline'].search([])
+        myrec.selitem=False
+
+    def selectbtn(self):
+        A=1
+
+class NewebConSelLine1(models.Model):
+    _name = "neweb_contract.contract_selline"
+    _description = "舊約勾選項目LINE"
+
+    sel_id = fields.Many2one('neweb_contract.contract_sel', string="勾選項目")
+    contract_id = fields.Integer(string="合約ID")
+    prod_set = fields.Many2one('neweb.prodset', string="產品組別")
+    prod_brand = fields.Many2one('neweb.prodbrand', string="品牌")
+    prod_modeltype = fields.Char(string="機種-機型/料號")
+    prod_modeltype1 = fields.Many2one('neweb.sitem_modeltype1', string="機型名稱")
+    machine_serial_no = fields.Char(string='序號')
+    rack_loc = fields.Char(string="櫃位")
+    warranty_duedate = fields.Date(string="原廠保固到期日")
+    server_name = fields.Char(string="主機名稱")
+    machine_used_desc = fields.Char(string="設備用途說明")
+    prod_line_os = fields.Text(string="作業系統")
+    expand_module = fields.Char(string="擴充模組")
+    machine_other = fields.Char(string="其他")
+    machine_loc = fields.Char(string="設備位址")
+    contract_line_id = fields.Integer(string="Origin ID")
+    selitem = fields.Boolean(string="勾選",default=False)
+
+    def sel_button(self):
+        if self.selitem==False:
+            self.selitem=True
+        else:
+            self.selitem=False
+
+
